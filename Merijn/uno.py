73ed1e6be1987fba53acbcdeac7bdfcc2ca70944
CB101b <merijnsandvliet7@gmail.com>
2019-04-23 12:31:31 +0200

    Add intro

--- Merijn/uno.py
+++ Merijn/uno.py
@@ -1,3 +1,18 @@
+# imports
 import json
+import math
+import random
+
+# load JSON files
 cards = json.load(open('cards.json','r'))
 
+# declare card color sets
+blue = cards["blue"]
+red = cards["red"]
+yellow = cards["yellow"]
+green = cards["green"]
+
+print("Welcome to Uno!")
+input("Press any key to continue...")
+
+for 
